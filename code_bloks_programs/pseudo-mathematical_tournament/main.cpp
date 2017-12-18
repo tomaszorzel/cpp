1246fa872ac9596ecf074bd053f119c79f52a9b2
Tomasz Orzel <torzel92@gmail.com>
2017-12-18 14:11:14 +0100

    [SPOJ]  Pseudo-mathematical tournament - completed

--- code_bloks_programs/pseudo-mathematical_tournament/main.cpp
+++ code_bloks_programs/pseudo-mathematical_tournament/main.cpp
@@ -5,28 +5,38 @@ using namespace std;
 
 int main()
 {
-    int t, n;
+    int t, howManyNumbers;
     cin >> t;
     while (t--) {
-        cin >> n;
+        cin >> howManyNumbers;
         int number;
         vector<int> arr;
-        for (int i=0; i<n; i++) {
+        for (int i=0; i<howManyNumbers; i++) {
             cin >> number;
             arr.push_back(number);
         }
         sort(arr.begin(), arr.end());
         int max = *max_element(arr.begin(), arr.end());
-        for (vector<int>::iterator it=arr.begin(), int i=0; it!=arr.end(); it++, i++) {
-            cout << *it << endl;
-            if (*it==max && it!=arr.end()) {
-                cout << *it << " ";
-            } else if (*it==max) {
-                cout << *it << endl;
+        int counter=0;
+        for (unsigned i=0; i<arr.size(); i++) {
+            if (arr.at(i)==max) {
+                if (counter==arr.size()-1)
+                    cout << arr.at(i);
+                else
+                    cout << arr.at(i) << " ";
+                counter++;
             }
         }
 
-
+        for (unsigned i=0; i<arr.size(); i++) {
+            if (arr.at(i)!=max) {
+                if (counter<arr.size())
+                    cout << arr.at(i) << " ";
+                else
+                    cout << arr.at(i);
+            }
+        }
+        cout << endl;
     }
     return 0;
 }
